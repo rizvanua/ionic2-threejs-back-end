c439a77bc01216c9b3b1950f09c9384416fba1be
rizvanua <rizvanua@gmail.com>
2017-01-24 22:12:40 +0200

    init

--- mongoose.js
+++ mongoose.js
@@ -2,7 +2,7 @@
  * Created by Roman on 04.01.2017.
  */
 const mongoose = require('mongoose');
-const url = 'mongodb://localhost:27017/IonicThreeJsDB';
+const url = 'mongodb://localhost:27017/IonicThreeJsDB';// change this to your real mongodb path
 mongoose.connect(url);
 mongoose.Promise=global.Promise;
 
